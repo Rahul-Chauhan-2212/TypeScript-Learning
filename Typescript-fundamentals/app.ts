1b39aa018f80955ec0e0ae9f70307c82e460dac2
Rahul Chauhan <rchauhan9102@gmail.com>
2022-04-21 20:46:42 +0530

    Type Inference and Type Assignment

--- Typescript-fundamentals/app.ts
+++ Typescript-fundamentals/app.ts
@@ -6,9 +6,12 @@ function add(n1: number, n2: number, showResult: boolean, phrase: string) {
   }
 }
 
-const number1 = 5;
+//let number1 = 5; //type inference
+let number1:number; //type assignment
+number1=5;
 const number2 = 2.8;
 const printResult = true;
 const resultPhrase = "The Result is : ";
 
 add(number1, number2, printResult, resultPhrase);
+
